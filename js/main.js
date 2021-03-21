d9a0be5d0d1382448d439af8ade50f86de1a000a
minhoo03 <minhoo0333@gmail.com>
2021-03-21 19:22:22 +0900

    엑셀 데이터로 지도 표시

--- js/main.js
+++ js/main.js
@@ -1,67 +1,70 @@
-var parseData = JSON.parse(JSON.stringify(Params));
-// console.log(parseData[0].주소)
+let parseData = JSON.parse(JSON.stringify(Params));
 
-var address_parseData = parseData[0].주소
-
-const mainMap = () => {
 
 let mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        center: new kakao.maps.LatLng(37.538921, 126.990606), // 지도의 중심좌표
         level: 8 // 지도의 확대 레벨
     };  
-    // 37.591203, 127.086295
+// 37.591203, 127.086295
 let map = new kakao.maps.Map(mapContainer, mapOption); 
 
+// 마커 클러스터러를 생성합니다 
+let clusterer = new kakao.maps.MarkerClusterer({
+    map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
+    averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
+    minLevel: 10 // 클러스터 할 최소 지도 레벨 
+});
 
 
-let positions = [
-    {
-        title: '카카오', 
-        latlng: new kakao.maps.LatLng(33.450705, 126.570677)
-    },
-    {
-        title: '생태연못', 
-        latlng: new kakao.maps.LatLng(33.450936, 126.569477)
-    },
-    {
-        title: '근린공원',
-        latlng: new kakao.maps.LatLng(33.451393, 126.570738)
-    },
-    {
-        title: '텃밭', 
-        latlng: new kakao.maps.LatLng(33.450879, 126.569940)
-    },
 
-];
+const setMarker_data = (address, address_name) => {
 
 
-let geocoder = new kakao.maps.services.Geocoder();
-
-geocoder.addressSearch(address_parseData, function(result, status) {
-    console.log('1',result[0].x)
-
-        let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-        console.log('2',coords)
-        positions.push({
-            title: result[0].address_name,
-            latlng: coords
-        })
-
-        for (let i = 0; i < positions.length; i ++) {
-
-            // 마커를 생성합니다
-            let marker = new kakao.maps.Marker({
-                map: map, // 마커를 표시할 지도
-                position: positions[i].latlng, // 마커를 표시할 위치
-                title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-            });
-    
-            // 마커가 지도 위에 표시되도록 설정합니다
-            marker.setMap(map);
+    let positions = [
+        {
+            title: '서울 디지텍 고등학교', 
+            latlng: new kakao.maps.LatLng(37.538921, 126.990606)
         }
-});   
-console.log('3',positions)
+    ];
 
+
+
+    let geocoder = new kakao.maps.services.Geocoder();
+
+    geocoder.addressSearch(address, function(result, status) {
+        // console.log('1',result[0].x)
+
+            let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+            // console.log('2',coords)
+            positions.push({
+                title: address_name,
+                latlng: coords
+            })
+
+            for (let i = 0; i < positions.length; i ++) {
+
+                // 마커를 생성합니다
+                let marker = new kakao.maps.Marker({
+                    map: map, // 마커를 표시할 지도
+                    position: positions[i].latlng, // 마커를 표시할 위치
+                    title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                });
+        
+                // 마커가 지도 위에 표시되도록 설정합니다
+                marker.setMap(map);
+            }
+
+            
+    });   
+    // console.log('3',positions)
 }
-mainMap()
+
+
+for(let i = 0; i <= parseData.length; i++) {
+    let address_parseData = parseData[i].주소
+    let address_name_parseData = parseData[i].중학교명
+
+    setMarker_data(address_parseData ,address_name_parseData)
+    
+}
